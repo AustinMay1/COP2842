df47ec4d4a881bd7067811ef0827f0f109e89b90
austin.may1 <austin.may@protonmail.com>
2022-12-10 10:33:33 -0500

    init

---
+++ programClasses.php
@@ -0,0 +1,53 @@
+<?php
+
+class Instructor {
+    private $id;
+    private $firstName;
+    private $lastName;
+
+    public function __construct($id, $firstName, $lastName)
+    {
+        $this->id = $id;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+    }
+    
+    public function setFirstName($firstName) 
+    {
+        if($firstName) 
+        {
+            $this->firstName = $firstName;
+        }
+    }
+
+    public function setLastName($lastName)
+    {
+        if($lastName)
+        {
+            $this->lastName = $lastName;
+        }
+    }
+
+    public function getId() 
+    {
+        return $this->id;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function getFullName()
+    {
+        return $this->firstName + " " + $this->lastName;
+    }
+}
+
+
+?>
