e5886ab6119a6f10a790e2b4fdf0bb16f8482206
austin.may1 <austin.may@protonmail.com>
2022-12-11 11:52:33 -0500

    fetch class data for instructor from form value

--- instructorInfoForm.php
+++ instructorInfoForm.php
@@ -26,7 +26,7 @@ catch (PDOException $e)
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel='stylesheet' href='styles.css'/>
-    <title>Instructor Info</title>
+    <title>Select Instructor</title>
 </head>
 <body>
     <div>
--- instructorSchedule.php
+++ instructorSchedule.php
@@ -6,10 +6,16 @@ $connection = "mysql:host=localhost;dbname=classschedules;charset=utf8mb4";
 $user = "root";
 $password = "";
 $err = "";
-$instructors = array();
+$instructor = "";
 try
 {
     $pdo = new PDO($connection, $user, $password);
+    if(isset($_POST["instructor"]))
+    {
+        $instructor = $_POST["instructor"];
+        if(empty($instructor)) { $err = "No instructor selected."; }
+        else { $err = "Fatal error."; }
+    }
     
 }
 catch (PDOException $e) 
@@ -28,6 +34,58 @@ catch (PDOException $e)
     <title>Instructor Schedule</title>
 </head>
 <body>
-    
+    <?php
+        if(!$err) { echo $err; }
+    ?>
+    <div>
+    <?php
+        $query = "SELECT instructorid, instructorfirst, instructorlast FROM instructors WHERE instructorid = :ID";
+        $statement = $pdo->prepare($query);
+        $statement->execute(['ID' => $instructor]);
+        $teacher = $statement->fetch();
+        $schedule = new Instructor($teacher[0], $teacher[1], $teacher[2]);
+        
+        
+        if(!$teacher) 
+        {
+            echo "<p>No instructor selected.</p>";
+        }
+        else
+        {
+    ?>
+    <div>
+            <?php echo "<h3>Instructor: " . $schedule->getId() . "</h3>";
+                  echo "<br>";  
+                  echo $schedule->getFullName();
+
+                  $classes = "SELECT classes.course, classes.section, classes.dept, classes.enrolled, classes.maxAllowed, titles.title 
+                              FROM classes
+                              INNER JOIN instructors ON classes.instructorid = instructors.instructorid
+                              INNER JOIN titles ON classes.titlecode = titles.titlecode
+                              WHERE instructors.instructorid = $instructor;";
+                  $stmt = $pdo->prepare($classes);
+                  $stmt->execute();
+                  foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $col) {
+                    if (!$col) {
+                        echo "<p>Nothing to display!</p>";
+                    } else {
+                        echo "<table>";
+                        foreach($col as $k => $v) {
+                            echo "<tr>";
+                            echo "<td>$k</td>";
+                            echo "<td>$v</td>";
+                            echo "</tr>";
+                        }
+                        echo "</table>";
+                    }
+                  } 
+            ?>
+    </div>
+    <?php
+        }
+        $pdo = null;
+    ?>
+    </div>
+    <a href='instructorInfoForm.php'>Back To Search</a>
 </body>
 </html>
