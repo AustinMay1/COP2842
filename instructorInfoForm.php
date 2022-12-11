15e077746279cefcf249d6d5b5f9a5fd4260ac26
austin.may1 <austin.may@protonmail.com>
2022-12-11 14:26:31 -0500

    added bootstrap styling, finished query table

--- instructorInfoForm.php
+++ instructorInfoForm.php
@@ -7,13 +7,9 @@ $user = "root";
 $password = "";
 $err = "";
 $instructors = array();
-try
-{
+try {
     $pdo = new PDO($connection, $user, $password);
-    
-}
-catch (PDOException $e) 
-{
+} catch (PDOException $e) {
     $err = $e->getMessage();
 }
 
@@ -21,55 +17,63 @@ catch (PDOException $e)
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel='stylesheet' href='styles.css'/>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Select Instructor</title>
 </head>
+<style>
+.container {
+    height: 100vh;
+}
+</style>
 <body>
-    <div>
-        <?php
-            if (!$err)
-            {
-                echo "<h3>Please select an instructor</h3>";
-            }
-            else
-            {
+    <div class='container d-flex justify-content-center align-items-center'>
+        <div class='card text-center d-flex' style='width: 30rem;'>
+            <?php
+            if (!$err) {
+                echo "<div class='card-header'>Please select an instructor</div>";
+            } else {
                 echo "<p>$err</p>";
             }
-        ?>
-    </div>
-    <?php
-        if(!$err) {
-            $query = "SELECT instructorid, instructorfirst, instructorlast FROM instructors";
-            $res = $pdo->query($query);
+            ?>
 
-            foreach($res->fetchAll(PDO::FETCH_ASSOC) as $row) {
-                $inst = new Instructor($row['instructorid'], $row['instructorfirst'], $row['instructorlast']);
-                $instructors[$row['instructorid']] = $inst;
-            }
-        
-    ?>
-    <form method="POST" action="instructorSchedule.php">
-        <label>Select Instructor: </label>
-            <select name="instructor">
-                <option></option>
-                <?php
-                    foreach($instructors as $key => $inst) {
-                        echo "<option value='" . $key . "'>";
-                        echo $inst->getFullName();
-                        echo "</option>";
-                    }
-                ?>
-            </select>
-            <button>Lookup</button>
-    </form>
+            <?php
+            if (!$err) {
+                $query = "SELECT instructorid, instructorfirst, instructorlast FROM instructors";
+                $res = $pdo->query($query);
+
+                foreach ($res->fetchAll(PDO::FETCH_ASSOC) as $row) {
+                    $inst = new Instructor($row['instructorid'], $row['instructorfirst'], $row['instructorlast']);
+                    $instructors[$row['instructorid']] = $inst;
+                }
+
+            ?>
+                <div class="card-body">
+                    <form method="POST" action="instructorSchedule.php">
+                        <select class='form-select' aria-label='Instructor Select' name="instructor">
+                            <option></option>
+                            <?php
+                            foreach ($instructors as $key => $inst) {
+                                echo "<option value='" . $key . "'>";
+                                echo $inst->getFullName();
+                                echo "</option>";
+                            }
+                            ?>
+                        </select>
+                        <button type="submit" class='btn btn-outline-primary mt-3'>Lookup</button>
+                    </form>
+                </div>
+        </div>
     <?php
             }
-        //}
-        $pdo = null;
+            //}
+            $pdo = null;
     ?>
+    </div>
 </body>
+
 </html>
--- instructorSchedule.php
+++ instructorSchedule.php
@@ -7,19 +7,17 @@ $user = "root";
 $password = "";
 $err = "";
 $instructor = "";
-try
-{
+try {
     $pdo = new PDO($connection, $user, $password);
-    if(isset($_POST["instructor"]))
-    {
+    if (isset($_POST["instructor"])) {
         $instructor = $_POST["instructor"];
-        if(empty($instructor)) { $err = "No instructor selected."; }
-        else { $err = "Fatal error."; }
+        if (empty($instructor)) {
+            $err = "No instructor selected.";
+        } else {
+            $err = "Fatal error.";
+        }
     }
-    
-}
-catch (PDOException $e) 
-{
+} catch (PDOException $e) {
     $err = $e->getMessage();
 }
 
@@ -27,65 +25,84 @@ catch (PDOException $e)
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Instructor Schedule</title>
 </head>
-<body>
-    <?php
-        if(!$err) { echo $err; }
-    ?>
-    <div>
-    <?php
-        $query = "SELECT instructorid, instructorfirst, instructorlast FROM instructors WHERE instructorid = :ID";
-        $statement = $pdo->prepare($query);
-        $statement->execute(['ID' => $instructor]);
-        $teacher = $statement->fetch();
-        $schedule = new Instructor($teacher[0], $teacher[1], $teacher[2]);
-        
-        
-        if(!$teacher) 
-        {
-            echo "<p>No instructor selected.</p>";
-        }
-        else
-        {
-    ?>
-    <div>
-            <?php echo "<h3>Instructor: " . $schedule->getId() . "</h3>";
-                  echo "<br>";  
-                  echo $schedule->getFullName();
 
-                  $classes = "SELECT classes.course, classes.section, classes.dept, classes.enrolled, classes.maxAllowed, titles.title 
+<body>
+    <div class="container">
+        <?php
+        if (!$err) {
+            echo $err;
+        }
+        ?>
+        <div>
+            <?php
+            $query = "SELECT instructorid, instructorfirst, instructorlast FROM instructors WHERE instructorid = :ID";
+            $statement = $pdo->prepare($query);
+            $statement->execute(['ID' => $instructor]);
+            $teacher = $statement->fetch();
+            $teacherInfo = new Instructor($teacher[0], $teacher[1], $teacher[2]);
+
+
+            if (!$teacher) {
+                echo "<p>No instructor selected.</p>";
+            } else {
+            ?>
+                <div class='container'>
+                    <div class="card text-center">
+                    <div class='card-header'><h3><?php echo $teacherInfo->getFullName() . " (ID: " . $teacherInfo->getId() . ")"; ?></h3></div>
+                    <div class='card-body'>
+                    <table class='table table-bordered border-dark'>
+                        <thead>
+
+                            <?php
+                            $classes = "SELECT classes.course, classes.section, classes.dept, classes.enrolled, classes.maxAllowed, titles.title 
                               FROM classes
                               INNER JOIN instructors ON classes.instructorid = instructors.instructorid
                               INNER JOIN titles ON classes.titlecode = titles.titlecode
                               WHERE instructors.instructorid = $instructor;";
-                  $stmt = $pdo->prepare($classes);
-                  $stmt->execute();
-                  foreach($stmt->fetchAll(PDO::FETCH_ASSOC) as $col) {
-                    if (!$col) {
-                        echo "<p>Nothing to display!</p>";
-                    } else {
-                        echo "<table>";
-                        foreach($col as $k => $v) {
-                            echo "<tr>";
-                            echo "<td>$k</td>";
-                            echo "<td>$v</td>";
-                            echo "</tr>";
-                        }
-                        echo "</table>";
-                    }
-                  } 
+                            $sch = $pdo->query($classes);
+
+                            echo "<tr>
+                                    <th>Course</th>
+                                    <th>Section</th>
+                                    <th>Department</th>
+                                    <th>Enrolled</th>
+                                    <th>Seats Free</th>
+                                    <th>Textbook</th>
+                                 </tr>";
+
+                            foreach ($sch as $col) {
+                                if (!$col) {
+                                    echo "<p>Nothing to display!</p>";
+                                } else {
+                                    echo
+                                    "<tr><td>" . $col['course'] .
+                                        "</td><td>" . $col['section'] .
+                                        "</td><td>" . $col['dept'] .
+                                        "</td><td>" . $col['enrolled'] .
+                                        "</td><td>" . $col['maxAllowed'] - $col['enrolled'] .
+                                        "</td><td>" . $col['title'] .
+                                        "</tr>";
+                                }
+                            }
+                            ?>
+                    </table>
+                    </div>
+                        </div>
+                </div>
+            <?php
+            }
+            $pdo = null;
             ?>
+        </div>
+        <a href='instructorInfoForm.php' class='btn btn-outline-primary mt-3'>Back To Search</a>
     </div>
-    <?php
-        }
-        $pdo = null;
-    ?>
-    </div>
-    <a href='instructorInfoForm.php'>Back To Search</a>
 </body>
 </html>
